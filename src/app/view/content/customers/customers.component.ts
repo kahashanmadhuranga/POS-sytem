7ed8361c26a97f75faea65e13b0033364ca07b3b
kahashanmadhuranga <hashanmadhuranga@gmail.com>
2019-06-21 18:43:00 +0530

    Customer window and Item window complete

--- src/app/view/content/customers/customers.component.ts
+++ src/app/view/content/customers/customers.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'app-customers',
@@ -8,18 +7,10 @@ import {NgxSpinnerService} from 'ngx-spinner';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor(private spinner: NgxSpinnerService) {
+  constructor() {
   }
 
   ngOnInit() {
-    setTimeout(() => {
-      this.spinner.show();
-    }, 1);
-
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 5000);
   }
 
 }
